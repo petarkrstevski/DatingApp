86012a0b5b4052fa34c0999ec5fa4054de98fd7d
petar.krstevski <petarkrstevski1989@gmail.com>
2019-10-22 10:15:45 +0200

    Section 7, Part 4 Angular routing guards

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -3,11 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberListsComponent } from './member-lists/member-lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'lists', component: ListsComponent },
-  { path: 'members', component: MemberListsComponent },
+  { path: 'members', component: MemberListsComponent, canActivate:[AuthGuard] },
   { path: 'messages', component: MessagesComponent },
   { path: '**', redirectTo:'home', pathMatch: 'full' }
 ];
