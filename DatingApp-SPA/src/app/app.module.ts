48355c6e287c1dd3d7a5991c1d8646b290a74210
petar.krstevski <petarkrstevski1989@gmail.com>
2019-10-22 09:25:00 +0200

    section 7 routing part 1

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,17 +1,23 @@
+import { AuthService } from './_services/auth.service';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 // tslint:disable-next-line: max-line-length
 import { NavComponent } from './nav/nav.component';
-import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { MemberListsComponent } from './member-lists/member-lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
 
+import {appRoutes} from './routes'
 
 
 @NgModule({
@@ -19,13 +25,17 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
       AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      MemberListsComponent,
+      MessagesComponent,
+      ListsComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
-      BsDropdownModule.forRoot()
+      BsDropdownModule.forRoot(),
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [
       AuthService,
---
+++ DatingApp-SPA/src/app/routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
+import { MemberListsComponent } from './member-lists/member-lists.component';
+import { MessagesComponent } from './messages/messages.component';
+
+export const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'lists', component: ListsComponent },
+  { path: 'members', component: MemberListsComponent },
+  { path: 'messages', component: MessagesComponent },
+  { path: '**', redirectTo:'home', pathMatch: 'full' }
+];
+
+
